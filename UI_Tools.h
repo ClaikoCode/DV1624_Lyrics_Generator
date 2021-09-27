b24850cbe20428602bd587407d11158a852c9e2c
ClaikoCode <jonathan.dellova42@gmail.com>
2021-09-27 17:06:22 +0200

    UI tool functions implemented
    Testing of these functions reside in main file.

--- UI_Tools.h
+++
@@ -1,4 +0,0 @@
-void Ask_For_String(char* question, char* answer);
-int Ask_For_Int(char* question);
-unsigned char Ask_Yes_Or_No(char* question); 
-void Print_Current_Song_Structure();
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "UI_Tools.h"
+#include "constants.h"
 
 // "Starting song creation"
 // What part? Verse? Chorus? Intro? ...
@@ -11,10 +12,21 @@
 int main()
 {
 
-  char* string_ptr = NULL;
-  Ask_For_String("Random", string_ptr);
+  char string_ptr[BUFFER_LENGTH] = "";
+  char single = 'a';
+  Ask_For_String("What part?", string_ptr);
 
   printf("String generated: %s\n", string_ptr);
+  printf("Character %d\n", single);
+
+  unsigned int int_ans;
+  int_ans = Ask_For_Int("Give me a number");
+  printf("Integer given back: %u\n", int_ans);
+
+  int char_ans;
+  char_ans = Ask_Yes_Or_No("Would you like cheese with that?");
+
+  printf("Bool ans: %d\n", char_ans);
 
   // PRint introduction
 
